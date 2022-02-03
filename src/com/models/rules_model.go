77997900ed4b6376c5255cbd0338eef2875521ed
pingping <805029402@qq.com>
2022-02-03 23:06:10 +0800

    定时任务

--- src/com/models/rules_model.go
+++ src/com/models/rules_model.go
@@ -1,6 +1,10 @@
 package models
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+)
 
 var Ma string
 
@@ -8,7 +12,34 @@ type FetchUpdateRule struct {
 	TbName string
 }
 
+type Rule struct {
+	RuleId, Order, Description, OP, Selector, Executor string
+}
+
+var results []*Rule
+
 func (h FetchUpdateRule) Run() {
 	Ma = h.TbName
+	cur := NewMgo(h.TbName).FindAll(0, 1000, 1)
+	var rs []*Rule
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem Rule
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rs = append(rs, &elem)
+	}
+	results = rs
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
 	log.Println(Ma)
 }
